13b0c0e1a9242cd61a97ef87c0d311c09f8dadf1
zahrasabila <zahra.sabila22@mhs.uinjkt.ac.id>
2025-02-05 11:31:07 +0700

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { alata, be_vietnam_pro } from "./fonts";
 import GithubCardShiny from "@/components/animata/card/github-card-shiny";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Zahra Sabila - Portfolio",
@@ -16,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${alata.variable} ${be_vietnam_pro.variable}`}>
       <body>
-        <GithubCardShiny className="">{children}</GithubCardShiny>
+        <GithubCardShiny className="">
+          {children}
+          <Analytics />
+        </GithubCardShiny>
       </body>
     </html>
   );
